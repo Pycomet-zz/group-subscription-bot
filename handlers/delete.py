1c28644191a2f6cc176a47eaffa93ec47659514d
Pycomet <alfredemmanuelinyang@gmail.com>
2021-12-27 00:47:04 +0100

    yet to test add & delete

--- handlers/delete.py
+++ handlers/delete.py
@@ -1,26 +1,26 @@
 from config import *
 from utils import *
 
-@bot.message_handler(commands=['adduser'])
-def addUser(msg):
+@bot.message_handler(commands=['removeuser'])
+def removeUser(msg):
     """
-    Add User And Schedule Removal Date
+    Removes A User From Group
     """
     message = msg.text.split(" ")
     user = client.get_entity(int(message[1]))
 
     if user is not None:
         # Schedule User
-        status = add_user(user.id)
+        status = remove_user(user.id)
         if status == True:
             bot.send_message(
                 msg.from_user.id,
-                "User Added Successfully"
+                "User Deleted Successfully"
             )
         else:
             bot.send_message(
                 msg.from_user.id,
-                "User Adding Failed"
+                "User Removal Failed"
             )
 
     else:
@@ -28,5 +28,3 @@ def addUser(msg):
             msg.from_user.id,
             "User Not Found"
         )
-
-        
